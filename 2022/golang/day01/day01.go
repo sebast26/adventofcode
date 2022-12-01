a2d52142a0b60a5af2cb00650c4cf5f1055bb820
Sebastian Górecki <sebastian.gorecki@olx.pl>
2022-12-01 21:09:45 +0100

    2022: solutions to day01

---
+++ 2022/golang/day01/day01.go
@@ -0,0 +1,43 @@
+package main
+
+import (
+	"bufio"
+	"os"
+	"sort"
+	"strconv"
+	"strings"
+)
+
+func main() {
+	in, err := os.Open("day01/input.txt")
+	if err != nil {
+		panic(err)
+	}
+	defer in.Close()
+
+	scanner := bufio.NewScanner(in)
+
+	var cal []int
+	var max, current int
+	for scanner.Scan() {
+		line := strings.Trim(scanner.Text(), " \n\r\t")
+		if line == "" {
+			if current > max {
+				max = current
+			}
+			cal = append(cal, current)
+			current = 0
+			continue
+		}
+		num, err := strconv.Atoi(line)
+		if err != nil {
+			panic(err)
+		}
+		current += num
+	}
+
+	sort.Ints(cal)
+
+	println(cal[len(cal)-1])
+	println(cal[len(cal)-1] + cal[len(cal)-2] + cal[len(cal)-3])
+}
---
+++ 2022/golang/go.mod
@@ -0,0 +1,3 @@
+module sgorecki.me/adventofcode/2022
+
+go 1.19
