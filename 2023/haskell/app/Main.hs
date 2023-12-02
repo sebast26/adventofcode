57745974df86b1dcee95c83457fd1c49e10f8874
Sebastian Górecki <gorecki.sebastian@gmail.com>
2023-12-02 14:04:12 +0100

    2023, day01: haskell second part solved

--- 2023/haskell/app/Main.hs
+++ 2023/haskell/app/Main.hs
@@ -1,10 +1,12 @@
 module Main where
 
-import qualified Day01 (calibrationValue)
+import qualified Day01 (calibrationValue, realCalibrationValue)
 
 main :: IO ()
 main = do
   contents <- readFile "inputs/01a"
   let calibrationLines = lines contents
       calibrationValues = map (Day01.calibrationValue) calibrationLines
+      realCalibrationValues = map (Day01.realCalibrationValue) calibrationLines
   print $ sum calibrationValues
+  print $ sum realCalibrationValues
--- 2023/haskell/src/Day01.hs
+++ 2023/haskell/src/Day01.hs
@@ -1,6 +1,7 @@
-module Day01 (calibrationValue) where
+module Day01 (calibrationValue, realCalibrationValue) where
 
 import Data.Char (isDigit, digitToInt)
+import Data.List (isPrefixOf, isSuffixOf)
 
 -- Haskell Lists
 -- if you have a list: abcdefgh, then
@@ -30,3 +31,39 @@ calibrationValue xs =
     fd * 10 + ld
     where fd = firstDigit xs
           ld = lastDigit xs
+
+realFirstDigit :: [Char] -> Int
+realFirstDigit xs
+    | isPrefixOf "one" xs = 1
+    | isPrefixOf "two" xs = 2
+    | isPrefixOf "three" xs = 3
+    | isPrefixOf "four" xs = 4
+    | isPrefixOf "five" xs = 5
+    | isPrefixOf "six" xs = 6
+    | isPrefixOf "seven" xs = 7
+    | isPrefixOf "eight" xs = 8
+    | isPrefixOf "nine" xs = 9
+    | isDigit first = digitToInt first
+    | otherwise = realFirstDigit $ tail xs
+    where first = head xs
+
+realLastDigit :: [Char] -> Int
+realLastDigit xs
+    | isSuffixOf "one" xs = 1
+    | isSuffixOf "two" xs = 2
+    | isSuffixOf "three" xs = 3
+    | isSuffixOf "four" xs = 4
+    | isSuffixOf "five" xs = 5
+    | isSuffixOf "six" xs = 6
+    | isSuffixOf "seven" xs = 7
+    | isSuffixOf "eight" xs = 8
+    | isSuffixOf "nine" xs = 9
+    | isDigit end = digitToInt end
+    | otherwise = realLastDigit $ init xs
+    where end = last xs
+
+realCalibrationValue :: [Char] -> Int
+realCalibrationValue xs = 
+    rf * 10 + rl
+    where rf = realFirstDigit xs
+          rl = realLastDigit xs
