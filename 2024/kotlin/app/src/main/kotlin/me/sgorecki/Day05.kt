b7f3beb8b3e68bc958861673fba404a0b67bca4e
Sebastian Górecki <gorecki.sebastian@gmail.com>
2024-12-05 22:06:49 +0100

    2024: day5 wip

--- 2024/kotlin/app/src/main/kotlin/me/sgorecki/Day05.kt
+++ 2024/kotlin/app/src/main/kotlin/me/sgorecki/Day05.kt
@@ -1,11 +1,34 @@
 package me.sgorecki
 
 import java.io.File
+import kotlin.math.floor
+
+data class PageUpdate(val rules: List<Pair<Int, Int>>, val update: List<Int>) {
+    fun obeyRules(): Boolean {
+        return true
+    }
+
+    fun middlePageNumber(): Int {
+        return update[floor(update.count() / 2.0).toInt()]
+    }
+}
 
 fun main() {
-
     fun part1(input: List<String>): Int {
-        return 0;
+        val (rulesSection, updatesSection) = input.partition { it.contains("|") }
+        val rules = rulesSection.map { ruleLine ->
+            val pages = ruleLine.split("|")
+            pages.first().toInt() to pages.last().toInt()
+        }
+        val updates = updatesSection
+            .filterNot { it == "" }
+            .map { updateLine ->
+                updateLine.split(",").map { it.toInt() }
+            }
+
+        val pds = updates.map { PageUpdate(rules, it) }
+        return pds.filter { it.obeyRules() }
+            .sumOf { it.middlePageNumber() }
     }
 
     solve(::part1, "/Users/seba/projects/priv/code/adventofcode/2024/kotlin/inputs/05test.txt", 143)
