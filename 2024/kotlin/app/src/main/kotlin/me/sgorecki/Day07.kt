a358270a227a3d7d1374b4c039e100a55252f673
Sebastian Górecki <gorecki.sebastian@gmail.com>
2024-12-08 22:38:43 +0100

    2024: day7: part1 done

--- 2024/kotlin/app/src/main/kotlin/me/sgorecki/Day07.kt
+++ 2024/kotlin/app/src/main/kotlin/me/sgorecki/Day07.kt
@@ -1,24 +1,58 @@
 package me.sgorecki
 
+import me.sgorecki.Operation.ADD
+import me.sgorecki.Operation.MUL
 import java.io.File
 import java.math.BigInteger
 
 val inputRegex = """(?<res>\d+):(?<nums>(?:\s+\d+)*)""".toRegex()
 
-data class Equation(val result: BigInteger, val numbers: List<Int>)
-
-fun part1(input: List<String>): Int {
-    val equations = parseEquations(input)
-
-    return 0
+enum class Operation {
+    ADD, MUL
 }
+
+data class Equation(val expectedResult: BigInteger, val numbers: List<Int>) {
+    private val numberOfOperations = numbers.count() - 1
+
+    fun operationPermute(): List<List<Operation>> {
+        fun permute(temp: List<List<Operation>>, ops: List<Operation>): List<List<Operation>> {
+            if (ops.count() == numberOfOperations) {
+                return temp + listOf(ops)
+            }
+            val opsA = ops + ADD
+            val opsM = ops + MUL
+            val listA = permute(temp, opsA)
+            val listB = permute(temp, opsM)
+            return listA + listB
+        }
+        return permute(emptyList(), emptyList())
+    }
+
+    fun calculateResult(ops: List<Operation>): BigInteger {
+        return numbers.foldIndexed(numbers[0].toBigInteger()) { index, acc, num ->
+            if (index == 0) {
+                return@foldIndexed acc
+            }
+            return@foldIndexed when (ops[index - 1]) {
+                ADD -> acc + num.toBigInteger()
+                MUL -> acc * num.toBigInteger()
+            }
+        }
+    }
+
+    fun canBeCalculated() = operationPermute().any { calculateResult(it) == expectedResult }
+}
+
+fun part1(input: List<String>) = parseEquations(input)
+    .filter { it.canBeCalculated() }
+    .sumOf { it.expectedResult }
 
 private fun parseEquations(input: List<String>) = input.map { line ->
     val (res, nums) = inputRegex.matchEntire(line)
         ?.destructured
         ?: throw IllegalArgumentException("Incorrect input line $line")
     Equation(
-        result = res.toBigInteger(),
+        expectedResult = res.toBigInteger(),
         numbers = nums.split(" ")
             .filter { it != "" }
             .map { it.toInt() }
@@ -26,10 +60,11 @@ private fun parseEquations(input: List<String>) = input.map { line ->
 }
 
 fun main() {
-    solve(::part1, "/Users/seba/projects/priv/code/adventofcode/2024/kotlin/inputs/07sample.txt", 3749)
+    solve(::part1, "/Users/seba/projects/priv/code/adventofcode/2024/kotlin/inputs/07sample.txt", 3749.toBigInteger())
+    solve(::part1, "/Users/seba/projects/priv/code/adventofcode/2024/kotlin/inputs/07.txt", 2437272016585.toBigInteger())
 }
 
-private fun solve(resultFn: (List<String>) -> Int, input: String, expected: Int) {
+private fun solve(resultFn: (List<String>) -> BigInteger, input: String, expected: BigInteger) {
     val lines = File(input).readLines()
     val actual = resultFn(lines)
     if (actual == expected) {
