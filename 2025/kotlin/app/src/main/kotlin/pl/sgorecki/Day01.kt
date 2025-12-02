eb8651eb6fa570da3dd542fd98a25de2fc494d92
Sebastian Górecki <gorecki.sebastian@gmail.com>
2025-12-02 21:22:44 +0100

    2025: kotlin: day01: part2: refactor

--- 2025/kotlin/app/src/main/kotlin/pl/sgorecki/Day01.kt
+++ 2025/kotlin/app/src/main/kotlin/pl/sgorecki/Day01.kt
@@ -2,8 +2,6 @@ package pl.sgorecki
 
 import java.io.File
 import kotlin.math.abs
-import kotlin.math.floor
-
 
 sealed interface Rotator {
     fun rotateFrom(initialValue: Int): Int
@@ -39,38 +37,36 @@ fun main() {
         var current = 50
         return input.map { command ->
             val n = command.substring(1).toInt()
-            if (command.startsWith('L')) {
-                current = LeftRotator(n).rotateFrom(current)
+            current = if (command.startsWith('L')) {
+                LeftRotator(n).rotateFrom(current)
             } else {
-                current = RightRotator(n).rotateFrom(current)
+                RightRotator(n).rotateFrom(current)
             }
             current
         }.count { it == 0 }
     }
 
+    fun part02(input: List<String>): Int {
+        var current = 50
+        var zeroPoint = 0
+        input.map { command ->
+            val n = command.substring(1).toInt()
+            if (command.startsWith('L')) {
+                zeroPoint += LeftRotator(n).zeroPassesFrom(current)
+                current = LeftRotator(n).rotateFrom(current)
+            } else {
+                zeroPoint += RightRotator(n).zeroPassesFrom(current)
+                current = RightRotator(n).rotateFrom(current)
+            }
+        }
+        return zeroPoint
+    }
 
     solve(::part01, "/Users/seba/code/adventofcode/2025/kotlin/inputs/01sample.txt", 3)
     solve(::part01, "/Users/seba/code/adventofcode/2025/kotlin/inputs/01.txt", 1102)
-    solve(::day01part02, "/Users/seba/code/adventofcode/2025/kotlin/inputs/01sample.txt", 6)
-    solve(::day01part02, "/Users/seba/code/adventofcode/2025/kotlin/inputs/01.txt", 6175)
+    solve(::part02, "/Users/seba/code/adventofcode/2025/kotlin/inputs/01sample.txt", 6)
+    solve(::part02, "/Users/seba/code/adventofcode/2025/kotlin/inputs/01.txt", 6175)
 }
-
-fun day01part02(input: List<String>): Int {
-    var current = 50
-    var zeroPoint = 0
-    input.map { command ->
-        val n = command.substring(1).toInt()
-        if (command.startsWith('L')) {
-            zeroPoint += LeftRotator(n).zeroPassesFrom(current)
-            current = LeftRotator(n).rotateFrom(current)
-        } else {
-            zeroPoint += RightRotator(n).zeroPassesFrom(current)
-            current = RightRotator(n).rotateFrom(current)
-        }
-    }
-    return zeroPoint
-}
-
 
 private fun <E> solve(resultFn: (List<String>) -> E, input: String, expected: E) {
     val lines = File(input).readLines()
