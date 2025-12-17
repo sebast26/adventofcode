f87573e05d0168c18ee696907966dcda4f4523bc
Sebastian Górecki <gorecki.sebastian@gmail.com>
2025-12-17 23:21:59 +0100

    2020: kotlin: refactor utils

--- 2020/kotlin/src/main/kotlin/pl/sgorecki/common/solve.kt
+++ 2020/kotlin/src/main/kotlin/pl/sgorecki/common/solve.kt
@@ -1,5 +1,11 @@
 package pl.sgorecki.common
 
+import java.nio.file.Path
+import kotlin.io.path.readLines
+
+fun readLines(name: String) =
+    Path.of("${System.getProperty("projectDir")}/inputs/$name").readLines()
+
 fun <E> solve(resultFn: (List<String>) -> E, input: String, expected: E) {
     val lines = readLines(input)
     val actual = resultFn(lines)
