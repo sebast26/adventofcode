10d1a924f36dccc4017c663b30387e68e3ba593c
Sebastian Górecki <gorecki.sebastian@gmail.com>
2025-12-18 21:44:50 +0100

    2015: kotlin: day01: done

--- 2015/kotlin/src/main/kotlin/pl/sgorecki/day01/Day01.kt
+++ 2015/kotlin/src/main/kotlin/pl/sgorecki/day01/Day01.kt
@@ -4,6 +4,7 @@ import pl.sgorecki.common.solve
 
 fun main() {
     solve(::part1, "01.txt", 74)
+    solve(::part2, "01.txt", 74)
 }
 
 fun part1(lines: List<String>): Int {
@@ -18,4 +19,17 @@ fun part1(lines: List<String>): Int {
             else -> 0
         }
     }
+}
+
+fun part2(lines: List<String>): Int {
+    val line = lines.first()
+    return line.foldIndexed(0 to 0) { idx, acc, ch ->
+        if (acc.first == -1) return@foldIndexed acc
+        val move = when (ch) {
+            '(' -> 1
+            ')' -> -1
+            else -> 0
+        }
+        acc.first + move to idx + 1
+    }.second
 }
