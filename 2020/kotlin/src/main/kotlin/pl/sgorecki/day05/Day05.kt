33273f9d0ca4fc6b67d9fc738bede0bd62d8e5a3
Sebastian Górecki <gorecki.sebastian@gmail.com>
2025-12-22 20:00:41 +0100

    2020: kotlin: day05: done

--- 2020/kotlin/src/main/kotlin/pl/sgorecki/day05/Day05.kt
+++ 2020/kotlin/src/main/kotlin/pl/sgorecki/day05/Day05.kt
@@ -4,9 +4,20 @@ import pl.sgorecki.common.solve
 
 fun main() {
     solve(::part1, "05.txt", 953)
+    solve(::part2, "05.txt", 615)
 }
 
 fun part1(lines: List<String>) = lines.maxOf { it.seat() }
+
+fun part2(lines: List<String>): Int {
+    val seatIds = lines.map { it.seat() }
+    val min = seatIds.min()
+    val max = seatIds.max()
+    (min..max).forEach { num ->
+        if (num !in seatIds) return num
+    }
+    return 0
+}
 
 private fun String.row(): Int {
     val mask = take(7).map { ch ->
